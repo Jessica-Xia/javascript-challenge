5830f2fc242aaad9bfdd5d8b96440488b13477e2
Jessica Gao CN <binqiang@marshall.usc.edu>
2020-01-04 19:30:35 -0800

    complete

--- UFO-level-2/static/js/app.js
+++ UFO-level-2/static/js/app.js
@@ -13,60 +13,62 @@ var $reset = document.querySelector("#reset");
 $search.addEventListener("click", SearchButtonClick);
 $reset.addEventListener("click", ResetButtonClick);
 
-
-
 var filteredData = data;
 
 function SearchButtonClick() {
+  
   var filterDate = $dateInput.value.trim();
   var filterCity = $cityInput.value.trim().toLowerCase();
   var filterState = $stateInput.value.trim().toLowerCase();
   var filterCountry = $countryInput.value.trim().toLowerCase();
   var filterShape = $shapeInput.value.trim().toLowerCase();
 
+
   if (filterDate != "") {
-      filteredData = filteredData.filter(function (date) {
-      var dataDate = date.datetime;
-      return dataDate === filterDate;
-  });
+    filteredData = filteredData.filter(function (date) {
+    var dataDate = date.datetime;
+    return dataDate === filterDate;
+});
 
-  }
+}
 
-  if (filterCity != "") {
-      filteredData = filteredData.filter(function (city) {
-      var dataCity = city.city;
-      return dataCity === filterCity;
-  });
-  }
+if (filterCity != "") {
+    filteredData = filteredData.filter(function (city) {
+    var dataCity = city.city;
+    return dataCity === filterCity;
+});
+}
 
-  if (filterState != "") {
-      filteredData = filteredData.filter(function (state) {
-          var dataState = state.state;
-          return dataState === filterState;
-      });
-  }
+if (filterState != "") {
+    filteredData = filteredData.filter(function (state) {
+        var dataState = state.state;
+        return dataState === filterState;
+    });
+}
 
-  if (filterCountry != "") {
-      filteredData = filteredData.filter(function (country) {
-          var dataCountry = country.country;
-          return dataCountry === filterCountry;
-      });
-  }
+if (filterCountry != "") {
+    filteredData = filteredData.filter(function (country) {
+        var dataCountry = country.country;
+        return dataCountry === filterCountry;
+    });
+}
 
-  if (filterShape != "") {
-      filteredData = filteredData.filter(function (shape) {
-          var dataShape = shape.shape;
-          return dataShape === filterShape;
-      });
-  }
+if (filterShape != "") {
+    filteredData = filteredData.filter(function (shape) {
+        var dataShape = shape.shape;
+        return dataShape === filterShape;
+    });
+}
+
 
   renderTable();
+
 }
 
 
 function renderTable() {
   $tbody.innerHTML = "";
-
+  
   for (var i = 0; i < filteredData.length; i++) {
     
     var address = filteredData[i];
@@ -88,3 +90,4 @@ function ResetButtonClick(){
 
 
 renderTable();
+
